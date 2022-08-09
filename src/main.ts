daf1fb96eee6e0d773b73f295587a05caf3a2f7c
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-08-09 18:55:28 +0900

    refactor: change style.css to style.scss

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import "bootstrap/dist/css/bootstrap.min.css"
-import "./style.css"
+import "./style.scss"
 
 createApp(App).mount('#app')
